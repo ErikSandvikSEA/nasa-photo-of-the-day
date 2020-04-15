6e1e2413c4097032027699ddc57a5d86981618b9
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-15 11:14:42 -0700

    initial commit with wireframe added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,48 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <header>
+        <nav className='header-nav'>
+          <a></a>
+          <a></a>
+          <a></a>
+        </nav>
+        <img/>
+        <h1></h1>
+      </header>
+      <section className="body">
+        <h2></h2>
+        <div>
+          <h4></h4>
+          <p>
+            Read through the instructions in the README.md file to build your NASA
+            app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+          </p>  
+        </div>
+        <div>
+          <h4></h4>
+          <p>
+            Read through the instructions in the README.md file to build your NASA
+            app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+          </p>  
+        </div>
+        <div>
+          <h4></h4>
+          <p>
+            Read through the instructions in the README.md file to build your NASA
+            app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+          </p>  
+        </div>
+
+      </section>
+      <footer>
+        <nav className='footer-nav'>
+          <a></a>
+          <a></a>
+          <a></a>
+        </nav>
+      </footer>
+    
     </div>
   );
 }
