d725582a006bb91b63dfffb6fc4afb22c5a042d7
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-15 11:22:05 -0700

    axios installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <header>
@@ -15,25 +17,23 @@ function App() {
       </header>
       <section className="body">
         <h2></h2>
-        <div>
+        <div className='component component1'>
           <h4></h4>
           <p>
             Read through the instructions in the README.md file to build your NASA
             app! Have fun <span role="img" aria-label='go!'>🚀</span>!
           </p>  
         </div>
-        <div>
+        <div className='component component2'>
           <h4></h4>
           <p>
-            Read through the instructions in the README.md file to build your NASA
-            app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+            Lorem Ipsum Blah blah
           </p>  
         </div>
-        <div>
+        <div className='component component3'>
           <h4></h4>
           <p>
-            Read through the instructions in the README.md file to build your NASA
-            app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+          Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
           </p>  
         </div>
 
